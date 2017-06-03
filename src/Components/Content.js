5ffe1e0ee77d0b752e024eb43e9f17dbada83058
Matt Lomet <mattlomet@live.com>
2017-06-03 15:19:25 -0400

    ugh not another bootcamp

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -3,6 +3,7 @@ import nycTerminal from "../nycterminal.PNG";
 import nycdaLogo from "../nycda.svg";
 import linkedIn from "../linked-in.png";
 import gitHub from "../github.png";
+import saltShaker from "../saltshaker.png";
 import "../Stylesheets/Content.css";
 
 function Content() {
@@ -39,7 +40,7 @@ function Content() {
           </div>
         </div>
       </div>
-      <div className="content-for-body-continued">
+      <div className="content-for-body-2">
         <div className="content-section2-div">
           <div className="content-section2-languages">
             <div className="html language">HTML</div>
@@ -51,8 +52,19 @@ function Content() {
           </div>
           <div className="content-section2-header">My Skills</div>
           <div className="content-section2-body">
-            Like I stated above most of my skills as of right now are from NYCDA's web development intensive bootcamp. We focused on many different languages throughout out the course, working on team projects and learning the techniques ourselves with our teacher being a vital crutch. Please follow the link to see a break down of where I believe my skills are at. Aswell as my resume if that intrests you aswell.
+            Like I stated above most of my skills as of right now are from NYCDA's web development intensive bootcamp. We focused on many different languages throughout out the course, working on team projects and learning the techniques ourselves with our teacher being a vital crutch. Please follow the link to see a break down of where I believe my skills are at.
           </div>
+        </div>
+      </div>
+      <div className="content-for-body-3">
+        <div className="content-section3-div">
+          <div className="content-section3-header">
+            UGH! not another Bootcamp Grad
+          </div>
+          <div className="content-section3-body">
+            I'm clearly biased towards the system I picked so take this section with a grain of salt. On many forums and around the community its an unsettled topic of "are bootcamp graduates prepared to be junior developers?". Some have taken sides, some have stayed neutral. But I do understand the fear of employeers seeing a bunch of bootcamp graduates who can make "twitter clones" and not much more. In my opinion my bootcamp did not approach learning to program in this way, we learned how to aquire skills and apply them quickly. For many projects we worked in groups aswell and learned the challenges that comes along with that aswell.
+          </div>
+          <img className="saltShaker" src={saltShaker} />
         </div>
       </div>
     </div>
